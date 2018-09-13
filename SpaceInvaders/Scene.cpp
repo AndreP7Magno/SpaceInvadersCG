210196c0cdb4b6e614cef6eb775947fd0a137b8d
André Magno <andremagno14@gmail.com>
2018-09-12 21:57:27 -0300

    tentativa de uso de enums

--- SpaceInvaders/Scene.cpp
+++ SpaceInvaders/Scene.cpp
@@ -4,6 +4,8 @@
 
 Scene::Scene(int argc, char **argv, string title, int width, int height)
 {
+	Scene::SetTipoTela(Scene::TipoTela::Inicial); //TENTAR MUDAR O LUGAR DISSO
+
 	glutInit(&argc, argv);
 	// Indica que deve ser usado um unico buffer para armazenamento da imagem e representacao de cores RGB
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
@@ -47,13 +49,33 @@ void Scene::Inicio() {
 }
 
 void Scene::DesenhaCena(void) {
+	Scene::TipoTela telaAtual = Scene::GetTipoTela();
+
 	// Limpa a janela com a cor especificada como cor de fundo
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
+	//xmin, xmax, ymin, ymax
+	//gluOrtho2D(-2, 2, -1.2, 2.8);
+	
+	if (telaAtual == Scene::TipoTela::Inicial) {
 
+	}
+
+	telaAtual = Scene::GetTipoTela();
+	if (telaAtual == Scene::TipoTela::Ganhador) {
+
+	}
+
+	telaAtual = Scene::GetTipoTela();
+	if (telaAtual == Scene::TipoTela::Perdedor) {
+
+	}
+
+	glPopMatrix();
+	glutSwapBuffers();
 }
 
 
--- SpaceInvaders/Scene.h
+++ SpaceInvaders/Scene.h
@@ -3,6 +3,7 @@
 #include "GL\freeglut.h"
 #include <vector>
 using namespace std;
+using std::string;
 
 class Scene
 {
@@ -10,12 +11,34 @@ class Scene
 	int height;
 	string title;
 
-
 public:
+
+	enum TipoTela
+	{
+		Inicial,
+		Ganhador,
+		Perdedor		
+	};
+
+	static void Scene::SetTipoTela(Scene::TipoTela tipoTela)
+	{
+		_tipoTela = tipoTela;
+	}
+
+	static Scene::TipoTela Scene::GetTipoTela()
+	{
+		return _tipoTela;
+	}
+
+	static Scene::TipoTela _tipoTela;
+
 	Scene(int argc, char **argv, string title, int width, int height);
 	void Inicio();
 	static void DesenhaCena(void);
 	~Scene();
 
+private:
+	
 };
+
 
